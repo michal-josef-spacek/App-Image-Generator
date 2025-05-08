e87bac367b019c48dac846a24a4af85d2fe6cc19
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-05-08 11:02:46 +0200

    Prepare for 0.08 version

--- Generator.pm
+++ Generator.pm
@@ -17,7 +17,7 @@ use Readonly;
 Readonly::Scalar our $EMPTY_STR => q{};
 Readonly::Array our @PATTERNS => qw(checkerboard);
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 # Constructor.
 sub new {
@@ -230,6 +230,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
